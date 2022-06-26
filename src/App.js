cc4ef190c54e57b4045314b5d8c5d7f2c0d7557e
jenna-graham <jclayto2@gmail.com>
2022-06-26 12:37:06 -0700

    set up pages, added links

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from 'react-router-dom';
 import './App.css';
+import AuthPage from './AuthPage';
+import CreatePage from './CreatePage';
+import UpdatePage from './UpdatePage';
+import ListPage from './ListPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Sign In</Link>
+            </li>
+            <li>
+              <Link to="/create">Create New Book</Link>
+            </li>
+            <li>
+              <Link to="/books/1">Update a book</Link>
+            </li>
+            <li>
+              <Link to="/books">List of Books</Link>
+            </li>
+            <li>
+        
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route exact path="/">
+            <AuthPage />
+          </Route>
+          <Route exact path="/books/:id">
+            <UpdatePage />
+          </Route>
+          <Route path="/books">
+            <ListPage />
+          </Route>
+          <Route exact path="/create">
+            <CreatePage />
+          </Route>
+        </Switch>
+      </div>
+
+    </Router>
+     
   );
 }
 
